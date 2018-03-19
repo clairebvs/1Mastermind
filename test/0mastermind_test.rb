6a707797d0c2a6d68321c5d7286c4768a2a795fe
ClaireBvs <clairembeauvais@gmail.com>
2018-03-19 06:59:12 -0600

    new test

---
+++ test/0mastermind_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/pride'
+require './lib/conways'
+
+class MastermindTest < Minitest::Test
+  def test_it_exists
+    game = Instruction.new()
+
+    assert_instance_of Instruction, game
+  end
+
+  def test_it_has_a_solution
+    game = Sequence.new
+
+    assert_instance_of Array, game.color_sample
+    assert color_sample
+
+    
+  end
